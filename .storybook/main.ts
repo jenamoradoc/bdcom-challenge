2833be4d03ec0cdd21ccbd69b61e6d4bea2e27e6
David Enamorado <davidenamorado09@gmail.com>
2026-05-21 10:02:51 -0300

    fix: remove addon-mcp to fix vercel

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -8,8 +8,7 @@ const config: StorybookConfig = {
   "addons": [
     "@chromatic-com/storybook",
     "@storybook/addon-a11y",
-    "@storybook/addon-docs",
-    "@storybook/addon-mcp"
+    "@storybook/addon-docs"
   ],
   "framework": "@storybook/nextjs-vite",
   "staticDirs": [
